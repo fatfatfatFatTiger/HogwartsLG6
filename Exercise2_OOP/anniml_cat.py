7281390291ba193382712ce20246e3c8e09a7a07
fatfatfatFatTiger <w475317858@163.com>
2021-01-24 17:00:31 +0800

    命令行上传

---
+++ Exercise2_OOP/anniml_cat.py
@@ -0,0 +1,31 @@
+
+from HogwartsLG6.Exercise2_OOP.animal import Animal
+
+
+class Cat(Animal):
+    gender = "female"
+
+    def __init__(self, name, color):
+        super().__init__(name, color)
+        self.hair = "short"
+
+    def catch(self):
+        mouse = "Dave"
+        print(f"I can catch mouse {mouse}")
+
+    def shout(self):
+        print(f"{self.name} can meow")
+
+
+if __name__ == "__main__":
+    dd = Cat("dingdang", "white")
+    # 调用父类方法
+    dd.introduce()
+    # 实例调用属性
+    print(f"my hair is {dd.hair}")
+    # 实例调用方法
+    dd.shout()
+    # 类调用方法
+    Cat("miumiu", "gray").catch()
+
+    # 测试修改内容后通过Pycharm提交到github
